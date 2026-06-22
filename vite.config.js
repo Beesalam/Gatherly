488de96c9a04494ca437cf13dcd9f15c557a6be3
Beesalam <abdulsalambabatunde305@gmail.com>
2026-06-22 13:25:44 +0100

    fix package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+// export default defineConfig({
+//  plugins: [react(), VitePWA(manifestForPlugIn)],
+ //})
+
+
+ export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Gatherly',
+        short_name: 'Gatherly',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
+
